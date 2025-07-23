f40755376832928b22a5d76df0f03f75024641fe
HieuGM <145418908+HieuGM@users.noreply.github.com>
2025-07-24 01:50:06 +0700

    Update server.py

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import google.generativeai as genai
 
-genai.configure(api_key="Key")
+genai.configure(api_key=st.secrets["Key"])
 
 model = genai.GenerativeModel("gemini-2.5-flash")
 
